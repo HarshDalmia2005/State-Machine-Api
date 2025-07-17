3bff9544cf5f9a62e8252ba1a86861aa2b0bb8ec
HarshDalmia <harshdalmia20212081@gmail.com>
2025-07-17 23:47:56 +0530

    minor fixes

--- state_machine_api/Program.cs
+++ state_machine_api/Program.cs
@@ -3,18 +3,16 @@ using state_machine_api.Storage;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 
-// Register in-memory storage and services
 builder.Services.AddSingleton<InMemoryStorage>();
 builder.Services.AddScoped<IWorkflowService, WorkflowService>();
 
 var app = builder.Build();
 
-// Configure middleware
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
